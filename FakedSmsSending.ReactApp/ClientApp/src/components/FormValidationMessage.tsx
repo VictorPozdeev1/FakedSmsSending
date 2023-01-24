8e16f19fd489189add248c48d9faecd678caaed3
VictorPozdeev1 <114474092+VictorPozdeev1@users.noreply.github.com>
2023-01-24 14:51:46 +0400

    Add loading+validation (#2)
    * Add loading message
    
    * Add client app validation
    
    * Fix form validation a bit
    
    * Add file to .csproj
    
    * Add server-side validation

---
+++ FakedSmsSending.ReactApp/ClientApp/src/components/FormValidationMessage.tsx
@@ -0,0 +1,8 @@
+﻿interface IFormValidationMessageProps {
+    message: string
+}
+export default function FormValidationMessage({ message }: IFormValidationMessageProps) {
+    return (
+        <p className='text-center text-xl text-red-600'>{message}</p>
+    )
+}
---
+++ FakedSmsSending.ReactApp/ClientApp/src/components/Loader.tsx
@@ -0,0 +1,5 @@
+﻿export default function Loader() {
+    return (
+        <p className='text-center text-slate-600 p-6 text-xl'>Loading...</p>
+    )
+}
