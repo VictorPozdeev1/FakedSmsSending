ce54dd87b49532ca999d879a18488fb74541c8ea
Victor Pozdeev <victorpozdeev@gmail.com>
2023-01-24 15:22:09 +0400

    Remove unnecessaries from ReactApp/Program.cs

--- FakedSmsSending.ReactApp/Program.cs
+++ FakedSmsSending.ReactApp/Program.cs
@@ -1,9 +1,4 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,12 +11,4 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html"); ;
-
 app.Run();
