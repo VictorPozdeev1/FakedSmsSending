816eca407d8b2336020bee8d8890f01bce416532
Victor Pozdeev <victorpozdeev@gmail.com>
2023-01-24 06:00:41 +0400

    Add server-side validation

--- FakedSmsSending.WebApi/Models/SmsToSend.cs
+++ FakedSmsSending.WebApi/Models/SmsToSend.cs
@@ -1,9 +1,14 @@
-﻿namespace Faked_sms_sending.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Faked_sms_sending.Models;
 
 public class SmsToSend
 {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    [Required(ErrorMessage = "Текст смс не должен быть пустым.")]
     public string Text { get; init; }
+    [Required(ErrorMessage = "Не указан номер телефона получателя.")]
+    [RegularExpression("^\\d{11}$", ErrorMessage = "Номер телефона должен состоять из 11 цифр.")]
     public string ReceiverPhoneNo { get; init; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public string? SenderName { get; init; }
